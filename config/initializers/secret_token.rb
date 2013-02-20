3a147fce7be41d33a91c92a8074ed3684f3a684f
TooHighToPlay <zil.kucinskas@gmail.com>
2013-02-20 02:15:00 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'bbecf6ffe86741d704fb66d3425e24d2a5063f6a8d080ce707f1a23fc552c5f787c0611a8dfcfcf10d02514360c81a89562d5ba7ae6e74477316b695601a2429'
